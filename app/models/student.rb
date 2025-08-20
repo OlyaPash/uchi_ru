38181a21938c480bf1470a7f717aef06d64b47b2
Olya <olya.pash.rb@gmail.com>
2025-08-20 13:48:17 +0500

    UchiRu-1 added students count

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-  belongs_to :classroom
+  belongs_to :classroom, counter_cache: :students_count
   belongs_to :school
 
   validates :first_name, :last_name, :surname, presence: true
